9313e5397178bb11cc57a0e26b13297766515b2c
Carsten Friedrich <carsten.friedrich@gmail.com>
2023-12-10 18:31:38 +1100

    Update from support jcenter() to mavenCentral() and from preference-v7 to preferencex

--- app/src/main/java/org/carsten/WordFinderSettingsFragment.java
+++ app/src/main/java/org/carsten/WordFinderSettingsFragment.java
@@ -1,13 +1,16 @@
 package org.carsten;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
 import android.text.InputFilter;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.widget.EditText;
 
-import com.takisoft.fix.support.v7.preference.EditTextPreference;
-import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+import androidx.annotation.NonNull;
+
+import com.takisoft.preferencex.EditTextPreference;
+import com.takisoft.preferencex.PreferenceFragmentCompat;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,19 +21,19 @@ public class WordFinderSettingsFragment extends PreferenceFragmentCompat {
 	public void onCreatePreferencesFix(Bundle savedInstanceState, String rootKey) {
 		setPreferencesFromResource(R.xml.preferences, rootKey);
 		EditTextPreference timeEdit = (EditTextPreference) findPreference("countdown_time_pref");
-		List<InputFilter> filters = new ArrayList<>(Arrays.asList(timeEdit.getEditText().getFilters()));
-		filters.add(new InputFilter() {
-			@Nullable
-            @Override
-			public CharSequence filter(CharSequence source, int start, int end,
-                                       Spanned dest, int dstart, int dend) {
-				SpannableStringBuilder builder = new SpannableStringBuilder(dest);
-				builder.replace(dstart, dend, source, start, end);
-				if( builder.toString().matches("[0-9]+(:(|[0-5]|[0-5][0-9]))?")) //     :?( [ ?[0-5]?[0-9]?"))
-					return null;
-				return "";
-			}
-		});
-		timeEdit.getEditText().setFilters(filters.toArray(new InputFilter[0]));
+
+		if(timeEdit!=null)
+			timeEdit.setOnBindEditTextListener( (@NonNull EditText editText) -> {
+					List<InputFilter> filters = new ArrayList<>(Arrays.asList(editText.getFilters()));
+					filters.add((CharSequence source, int start, int end, Spanned dest, int dstart,
+								 int dend) -> {
+							SpannableStringBuilder builder = new SpannableStringBuilder(dest);
+							builder.replace(dstart, dend, source, start, end);
+							if( builder.toString().matches("[0-9]+(:(|[0-5]|[0-5][0-9]))?")) //     :?( [ ?[0-5]?[0-9]?"))
+								return null;
+							return "";
+					});
+					editText.setFilters(filters.toArray(new InputFilter[0]));
+			});
 	}
 }
