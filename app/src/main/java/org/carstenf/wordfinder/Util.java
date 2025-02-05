9af06896150e07b697221fbd9cde829ee6517e49
Carsten Friedrich <carsten.friedrich@gmail.com>
2025-02-05 14:24:39 +1100

    Fixed regression bug due to min SDK change

--- app/src/main/java/org/carstenf/wordfinder/Util.java
+++ app/src/main/java/org/carstenf/wordfinder/Util.java
@@ -3,6 +3,7 @@ package org.carstenf.wordfinder;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
+import android.os.Build;
 
 public class Util {
     static public boolean isNetworkAvailable(Context context) {
@@ -10,6 +11,9 @@ public class Util {
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
         if (connectivityManager != null) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                    return true; // Can't really check so assume yes
+            }
             NetworkCapabilities capabilities =
                     connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
             return capabilities != null &&
