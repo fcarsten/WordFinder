dc4fc5451d0e7f4be7573c3c352abca859bd9d9a
Carsten Friedrich <carsten.friedrich@gmail.com>
2025-02-23 12:10:25 +1100

    Finished english language manual

--- app/src/main/kotlin/org/carstenf/wordfinder/InfoDialogFragment.kt
+++ app/src/main/kotlin/org/carstenf/wordfinder/InfoDialogFragment.kt
@@ -10,11 +10,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.viewpager2.widget.ViewPager2
+import org.carstenf.wordfinder.InfoDialogFragment.Companion.TAG
 import java.io.InputStream
 import java.util.Locale
 
@@ -34,17 +36,19 @@ class InfoDialogFragment : DialogFragment() {
             "Error loading HTML content: ${e.message}"
         }
     }
+    var dialogView : View? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = AlertDialog.Builder(requireContext())
-        val dialogView = requireActivity().layoutInflater.inflate(R.layout.dialog_info, null)
-        builder.setView(dialogView)
+        val builder = AlertDialog.Builder(requireContext(), android.R.style.Theme_Black_NoTitleBar_Fullscreen)
+        val lDialogView = requireActivity().layoutInflater.inflate(R.layout.dialog_info, null)
+        builder.setView(lDialogView)
 
-        val viewPager: ViewPager2 = dialogView.findViewById(R.id.view_pager)
-        val prevButton: Button = dialogView.findViewById(R.id.prev_button)
-        val nextButton: Button = dialogView.findViewById(R.id.next_button)
-        val closeButton: Button = dialogView.findViewById(R.id.close_button)
+        val viewPager: ViewPager2 = lDialogView.findViewById(R.id.view_pager)
+        val prevButton: Button = lDialogView.findViewById(R.id.prev_button)
+        val nextButton: Button = lDialogView.findViewById(R.id.next_button)
+        val closeButton: Button = lDialogView.findViewById(R.id.close_button)
 
+        dialogView = lDialogView
         val locale = Locale.getDefault().language
 
         val htmlAssetPath  = when (locale) {
@@ -95,19 +99,35 @@ class InfoDialogFragment : DialogFragment() {
             }
         })
 
-        return builder.create()
+        val res = builder.create()
+        return res
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val height = (resources.displayMetrics.heightPixels * 0.8).toInt()
+        val lDialogView = dialogView
+        if(lDialogView != null) {
+            lDialogView.layoutParams.height = height
+            lDialogView.requestLayout()
+            lDialogView.invalidate()
+            lDialogView.forceLayout()
+            lDialogView.bringToFront()
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        dialog?.window?.setLayout(
-            (resources.displayMetrics.widthPixels * 0.95).toInt(),
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
+        val height = (resources.displayMetrics.heightPixels * 0.8).toInt()
+        val lWindow = dialog?.window
+        if( lWindow !=null ){
+            lWindow.setLayout((resources.displayMetrics.widthPixels * 0.95).toInt(), height)
+            lWindow.setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND, WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        }
     }
 
     companion object {
-        private const val TAG = "WordFinder InfoDialog"
+        const val TAG = "WordFinder InfoDialog"
 
         fun showInfo(fragmentManager: androidx.fragment.app.FragmentManager) {
             val dialogFragment = InfoDialogFragment()
@@ -138,30 +158,10 @@ class InfoPagerAdapter(private val pages: List<PageData>) :
         fun bind(page: PageData) {
             // Use the newer Html.fromHtml method with FROM_HTML_MODE_LEGACY
             val markup = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                Html.fromHtml(page.htmlText, Html.FROM_HTML_MODE_LEGACY, { source ->
-                    try {
-                        val inputStream: InputStream = textView.context.assets.open(page.htmlAssetPath+"/"+source)
-                        val drawable = Drawable.createFromStream(inputStream, null)
-                        drawable?.setBounds(0, 0, drawable.intrinsicWidth*4, drawable.intrinsicHeight*4)
-                        drawable
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        null // Return null if the image cannot be loaded
-                    }
-                }, null)
+                Html.fromHtml(page.htmlText, Html.FROM_HTML_MODE_LEGACY, DialogImageHandler(page, textView),null)
             } else {
                 @Suppress("DEPRECATION")
-                Html.fromHtml(page.htmlText, { source ->
-                    try {
-                        val inputStream: InputStream = textView.context.assets.open(source)
-                        val drawable = Drawable.createFromStream(inputStream, null)
-                        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
-                        drawable
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        null // Return null if the image cannot be loaded
-                    }
-                }, null) // Fallback for older versions
+                Html.fromHtml(page.htmlText, DialogImageHandler(page, textView), null) // Fallback for older versions
             }
 
             textView.apply{
@@ -171,4 +171,40 @@ class InfoPagerAdapter(private val pages: List<PageData>) :
             }
         }
     }
+}
+
+class DialogImageHandler(val page: PageData, val textView: TextView): Html.ImageGetter {
+
+    override fun getDrawable(source: String?): Drawable? {
+        return try {
+            val inputStream: InputStream =
+                textView.context.assets.open(page.htmlAssetPath + "/" + source)
+            val drawable = Drawable.createFromStream(inputStream, null)
+            if(drawable==null) {
+                Log.e(TAG, "Drawable is null")
+                return null
+            }
+            val originalWidth = drawable.intrinsicWidth
+            val originalHeight = drawable.intrinsicHeight
+
+//            val textViewWidth = textView.width // Get the width of the TextView
+            val desiredWidth = originalWidth * 4
+
+            // Calculate the height to maintain the aspect ratio
+            val desiredHeight = originalHeight * 4
+
+            drawable.setBounds(
+                0,
+                0,
+                desiredWidth,
+                desiredHeight
+            )
+            drawable
+        } catch (e: Exception) {
+            Log.e(TAG, e.message, e)
+            return null
+        }
+
+    }
+
 }
