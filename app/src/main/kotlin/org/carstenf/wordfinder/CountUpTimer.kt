25a67b51506d5848c23b1ca6a0a26e9e67d273d0
Carsten Friedrich <carsten.friedrich@gmail.com>
2025-06-11 22:40:25 +1000

    Started adding stopwatch function

---
+++ app/src/main/kotlin/org/carstenf/wordfinder/CountUpTimer.kt
@@ -0,0 +1,25 @@
+package org.carstenf.wordfinder
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class CountUpTimer(private val onTick: (Long) -> Unit) {
+    private var job: Job? = null
+
+    fun start() {
+        job = CoroutineScope(Dispatchers.Main).launch {
+            var seconds = 0L
+            while (true) {
+                onTick(seconds++)
+                delay(1000L)
+            }
+        }
+    }
+
+    fun cancel() {
+        job?.cancel()
+    }
+}
